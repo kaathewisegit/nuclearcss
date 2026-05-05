6e639c92706a7f471bdbe16ff1b23b46e88edd7a
Andrej Kolčin <self@kaathewise.net>
2026-05-05 20:36:29 +0300

    Switch from plain JavaScript to TypeScript
    Turns out Node can run it just fine now.

--- scripts/publish.js
+++
@@ -1,9 +0,0 @@
-import { spawn } from "node:child_process"
-import { createRequire } from "node:module"
-
-const require = createRequire(import.meta.url)
-const { version } = require("../package.json")
-
-const tag = version.includes("-") ? ["--tag", "next"] : []
-
-spawn("npm", ["publish", ...tag], { stdio: "inherit" })
---
+++ scripts/publish.ts
@@ -0,0 +1,6 @@
+import { spawn } from "node:child_process"
+import pkg from "../package.json" with { type: "json" }
+
+const tag = pkg.version.includes("-") ? ["--tag", "next"] : []
+
+spawn("npm", ["publish", "--dry-run", ...tag], { stdio: "inherit" })
